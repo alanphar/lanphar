47a2c80ba587020736e5a5c281312c2fb1688ab7
alanphar <alanphar@yahoo.com>
2025-04-09 21:20:54 -0700

    Created contiguous_subarrays.py

---
+++ contiguous_subarrays.py
@@ -0,0 +1,20 @@
+def count_subarrays(arr):
+  subarrays = [1] * len(arr)
+  
+  for index, value in enumerate(arr):
+    left = index - 1
+    right = index + 1
+
+    for i in range(left, -1, -1):
+      if arr[i] < value:
+        subarrays[index] += 1
+      else:
+        break
+
+    for i in range(right, len(arr)):
+      if arr[i] < value:
+        subarrays[index] += 1
+      else:
+        break      
+
+  return subarrays
