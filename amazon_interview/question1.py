e137dd69a48aeb4647620be4a4b8a58f98aaf3fe
alanphar <alanphar@gmail.com>
2025-05-09 22:51:21 -0700

    amazon questions added

---
+++ amazon_interview/question1.py
@@ -0,0 +1,8 @@
+"""machineCapacity is a parameter list of size n. Each individual machine’s capacity is
+ an integer value in the array. The efficiency is evaluated using a comparison metric:
+ the sum of the absolute difference between consecutive machine capacities.
+ The task is to determine whether achieving the same sum of absolute difference is possible
+ by removing some machines from the sequence to streamline the operations.
+ If it is possible, return the minimum number of machines required in the sequence
+ to attain the same sum of the absolute difference between consecutive machine capacities.
+"""
---
+++ amazon_interview/question2.py
@@ -0,0 +1,7 @@
+"""Given a list of powers of n number of servers as an array of integers,
+find the maximum number of servers that can be selected from a set of servers
+that can be rearranged in a way that the absolute difference between power of two
+adjacent servers is equal to or less than 1. The first and last servers in the sequence
+are also considered adjacent.
+Powers can be duplicates.
+The function should be called getMaxServers that takes an array called powers."""
